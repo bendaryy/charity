dd6cf36edf104a68dc4dd79e74a06b211863532a
bendaryy <mohamedbendary1994@gmail.com>
2023-06-17 21:38:31 +0300

    kjads

--- resources/views/withdraw/create.blade.php
+++ resources/views/withdraw/create.blade.php
@@ -19,16 +19,16 @@
     <x-app-layout>
         <x-slot name="header">
             <form action="{{ route('withdraw.store') }}" method="POST">
-                @method("POST")
+                @method('POST')
                 @csrf
                 <h4 for="type" style="font-size: 20px;font-weight: bold;margin:10px">نوع الصرف</h4>
                 <div class="form-check" style="margin-bottom: 10px">
                     @foreach ($exchanges as $ex)
-                    <input class="form-check-input" type="radio" required value="{{ $ex->name }}" name="type">
-                    <label class="form-check-label">
-                        {{$ex->name}}
-                    </label><br>
-
+                        <input class="form-check-input" type="radio" required value="{{ $ex->name }}"
+                            name="type">
+                        <label class="form-check-label">
+                            {{ $ex->name }}
+                        </label><br>
                     @endforeach
                     <a href="{{ route('exchange.create') }}" class="btn btn-primary">إضافة نوع صرف جديد</a>
                 </div>
@@ -40,17 +40,17 @@
                 <h2>اختر المستفيد من الصرف</h2>
                 <select required name="details_id" id="details_id" class="select2 form-control" style="width: 30%">
                     <option value="" disabled selected="true">اختر المستفيد</option>
-                    @foreach($details as $detail)
-                    <option value="{{ $detail->id }}">
-                        <h2>الإسم :{{ $detail->name }}</h2>
-                        <h2> الرقم القومى : {{ $detail->NationalId }}</h2>
-                        <h2> عدد الإبناء : {{ $detail->personsNumbers }}</h2>
-                    </option>
+                    @foreach ($details as $detail)
+                        <option value="{{ $detail->id }}">
+                            <h2>الإسم :{{ $detail->name }}</h2>
+                            <h2> الرقم القومى : {{ $detail->NationalId }}</h2>
+                            <h2> عدد الإبناء : {{ $detail->personsNumbers }}</h2>
+                        </option>
                     @endforeach
                 </select>
 
                 <h4 for="type" style="font-size: 20px;font-weight: bold;margin:10px">تاريخ الصرف</h4>
-                <input type="date" required name="date" value="<?php echo date("Y-m-d");?>">
+                <input type="date" required name="date" value="<?php echo date(' Y-m-d'); ?>">
                 <h4 style="font-size: 20px;font-weight: bold;margin:10px"> التفاصيل</h4>
                 <textarea name="details" id="" cols="30" rows="10"></textarea>
                 <div style="background: black;color:white;margin:20px;pading:20px">
@@ -63,40 +63,42 @@
             </form>
         </x-slot>
     </x-app-layout>
-    <script src="https://code.jquery.com/jquery-3.5.1.js"
-        integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
+        crossorigin="anonymous"></script>
+    <script src="{{ asset('public/js/select2cdn.js') }}"></script>
     <script>
         $(document).ready(function() {
-$('.select2').select2();
-});
+            $('.select2').select2();
+        });
 
 
-$('#details_id').change(function() {
-    var selectPerson = $(this).val();
-   // $("#hamada").html("test");
-    $.ajax({
-      url: selectPerson,
-      method: 'GET',
-      success: function(response) {
-        // Display the response in the div element with the ID 'my-div'
-        $('#hamada').html("اخر تاريخ صرف بتاريخ : " + response['date']  + "<br>" + " العدد المصروف :  " + (response['value']) + "<br>" +  " نوع الصرف : " + response['type']);
-        console.log(response);
-        if( typeof response['date'] !== 'undefined'){
-            alert("اخر تاريخ صرف بتاريخ : " + response['date']  + '\n' + " العدد المصروف :  " + (response['value']) + '\n' +  " نوع الصرف : " + response['type'] )
-            
-        }else{
-            alert('لم يتم الصرف له من قبل')
-            $('#hamada').html("لم يتم الصرف له من قبل");
-        }
-      },
-      error: function(jqXHR, textStatus, errorThrown) {
-        // Handle the error case
-      }
-    });
-  });
+        $('#details_id').change(function() {
+            var selectPerson = $(this).val();
+            // $("#hamada").html("test");
+            $.ajax({
+                url: selectPerson,
+                method: 'GET',
+                success: function(response) {
+                    // Display the response in the div element with the ID 'my-div'
+                    $('#hamada').html("اخر تاريخ صرف بتاريخ : " + response['date'] + "<br>" +
+                        " العدد المصروف :  " + (response['value']) + "<br>" + " نوع الصرف : " +
+                        response['type']);
+                    console.log(response);
+                    if (typeof response['date'] !== 'undefined') {
+                        alert("اخر تاريخ صرف بتاريخ : " + response['date'] + '\n' +
+                            " العدد المصروف :  " + (response['value']) + '\n' + " نوع الصرف : " +
+                            response['type'])
 
-
+                    } else {
+                        alert('لم يتم الصرف له من قبل')
+                        $('#hamada').html("لم يتم الصرف له من قبل");
+                    }
+                },
+                error: function(jqXHR, textStatus, errorThrown) {
+                    // Handle the error case
+                }
+            });
+        });
     </script>
 </body>
 
